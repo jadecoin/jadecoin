c217a93c44be86eecc30613337c9617086ae2871
root <jadecoindev>
2014-08-07 17:02:18 -0400

    Initial COmmit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define JadeCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "JadeCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
